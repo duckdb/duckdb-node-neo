4cd0c7bc0c7b3f2c51f0c543ba116027d10322c9
Sebastien Vanvelthem <sebastien.vanvelthem@weareretail.ai>
2025-01-29 14:30:32 +0100

    feat: explicit loading of native binaries

--- bindings/pkgs/@duckdb/node-bindings/duckdb.js
+++ bindings/pkgs/@duckdb/node-bindings/duckdb.js
@@ -1,1 +1,26 @@
-module.exports = require(`@duckdb/node-bindings-${process.platform}-${process.arch}/duckdb.node`);
+const getRuntimePlatformArch = () => `${process.platform}-${process.arch}`;
+
+let native;
+
+switch(getRuntimePlatformArch()) {
+    case 'linux-x64':
+        native = require('@duckdb/node-bindings-linux-x64/duckdb.node');
+        break;
+    case 'linux-arm64':
+        native = require('@duckdb/node-bindings-linux-arm64/duckdb.node');
+        break;
+    case 'darwin-arm64':
+        native = require('@duckdb/node-bindings-darwin-arm64/duckdb.node');
+        break;
+    case 'darwin-x64':
+        native = require('@duckdb/node-bindings-darwin-x64/duckdb.node');
+        break;
+    case 'win32-x64':
+        native = require('@duckdb/node-bindings-win32-x64/duckdb.node');
+        break;
+    default:
+        throw new Error(`Unsupported platform: ${getRuntimePlatformArch()}`);
+}
+
+module.exports = native;
+
