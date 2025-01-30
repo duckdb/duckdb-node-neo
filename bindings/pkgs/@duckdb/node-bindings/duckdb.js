edac1a8ba2aa75c9d688243e82a1dba4f69ab7d3
Sébastien Vanvelthem <belgattitude@gmail.com>
2025-01-30 16:07:56 +0100

    chore: refactor based on review

--- bindings/pkgs/@duckdb/node-bindings/duckdb.js
+++ bindings/pkgs/@duckdb/node-bindings/duckdb.js
@@ -1,26 +1,24 @@
 const getRuntimePlatformArch = () => `${process.platform}-${process.arch}`;
 
-let native;
-
-switch(getRuntimePlatformArch()) {
-    case 'linux-x64':
-        native = require('@duckdb/node-bindings-linux-x64/duckdb.node');
-        break;
-    case 'linux-arm64':
-        native = require('@duckdb/node-bindings-linux-arm64/duckdb.node');
-        break;
-    case 'darwin-arm64':
-        native = require('@duckdb/node-bindings-darwin-arm64/duckdb.node');
-        break;
-    case 'darwin-x64':
-        native = require('@duckdb/node-bindings-darwin-x64/duckdb.node');
-        break;
-    case 'win32-x64':
-        native = require('@duckdb/node-bindings-win32-x64/duckdb.node');
-        break;
-    default:
-        throw new Error(`Unsupported platform: ${getRuntimePlatformArch()}`);
+/**
+ * @throw Error if there isn't any available native binding for the current platform/arch.
+ */
+const getNativeNodeBinding = (runtimePlatformArch) => {
+    switch(runtimePlatformArch) {
+        case `linux-x64`:
+            return require('@duckdb/node-bindings-linux-x64/duckdb.node');
+        case 'linux-arm64':
+            return require('@duckdb/node-bindings-linux-arm64/duckdb.node');
+        case 'darwin-arm64':
+            return require('@duckdb/node-bindings-darwin-arm64/duckdb.node');
+        case 'darwin-x64':
+            return require('@duckdb/node-bindings-darwin-x64/duckdb.node');
+        case 'win32-x64':
+            return require('@duckdb/node-bindings-win32-x64/duckdb.node');
+        default:
+            const [platform, arch] = runtimePlatformArch.split('-')
+            throw new Error(`Error loading duckdb native binding: unsupported arch '${arch}' for platform '${platform}'`);
+    }
 }
 
-module.exports = native;
-
+module.exports = getNativeNodeBinding(getRuntimePlatformArch());
