19840b0df5c83700a9e4848b64e9bc6e4f553838
Jeff Raymakers <jray@motherduck.com>
2025-03-02 11:24:58 -0800

    support default export in api in ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,26 +1,3 @@
-export {
-  hugeint_to_double,
-  double_to_hugeint,
-  uhugeint_to_double,
-  double_to_uhugeint,
-} from '@duckdb/node-bindings';
-export * from './DuckDBAppender';
-export * from './DuckDBConnection';
-export * from './DuckDBDataChunk';
-export * from './DuckDBExtractedStatements';
-export * from './DuckDBInstance';
-export * from './DuckDBLogicalType';
-export * from './DuckDBMaterializedResult';
-export * from './DuckDBPendingResult';
-export * from './DuckDBPreparedStatement';
-export * from './DuckDBResult';
-export * from './DuckDBType';
-export * from './DuckDBTypeId';
-export * from './DuckDBValueConverter';
-export * from './DuckDBValueToJsonConverter';
-export * from './DuckDBVector';
-export * from './configurationOptionDescriptions';
-export * from './enums';
-export * from './sql';
-export * from './values';
-export * from './version';
+import * as duckdb from './duckdb';
+export default duckdb;
+export * from './duckdb';
