661c05f3d4954314248fa468039067ae95bf9cc0
Nik <shevchenko.nik@icloud.com>
2025-03-26 22:17:37 +0100

    Fixed comments for ASCII charset

--- api/src/conversion/stringFromBlob.ts
+++ api/src/conversion/stringFromBlob.ts
@@ -3,10 +3,10 @@ export function stringFromBlob(bytes: Uint8Array): string {
   const byteStrings: string[] = [];
   for (const byte of bytes) {
     if (
-      byte <= 0x1f ||
-      byte === 0x22 /* single quote */ ||
-      byte === 0x27 /* double quote */ ||
-      byte >= 0x7f
+        byte <= 0x1f ||
+        byte === 0x22 /* double quote */ ||
+        byte === 0x27 /* single quote */ ||
+        byte >= 0x7f
     ) {
       byteStrings.push(`\\x${byte.toString(16).toUpperCase().padStart(2, '0')}`);
     } else {
