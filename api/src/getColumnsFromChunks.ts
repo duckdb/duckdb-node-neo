5230aa523cb891915587b5a1db4c457ec6f8ea98
Jeff Raymakers <jray@motherduck.com>
2025-03-29 16:29:16 -0700

    chunk objects methods

--- api/src/getColumnsFromChunks.ts
+++ api/src/getColumnsFromChunks.ts
@@ -5,16 +5,8 @@ export function getColumnsFromChunks(
   chunks: readonly DuckDBDataChunk[]
 ): DuckDBValue[][] {
   const columns: DuckDBValue[][] = [];
-  if (chunks.length === 0) {
-    return columns;
-  }
-  chunks[0].visitColumns((column) => columns.push(column));
-  for (let chunkIndex = 1; chunkIndex < chunks.length; chunkIndex++) {
-    for (let columnIndex = 0; columnIndex < columns.length; columnIndex++) {
-      chunks[chunkIndex].visitColumnValues(columnIndex, (value) =>
-        columns[columnIndex].push(value)
-      );
-    }
+  for (const chunk of chunks) {
+    chunk.appendToColumns(columns);
   }
   return columns;
 }
