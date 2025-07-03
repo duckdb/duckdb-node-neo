888c0deb72eabbfd6d8975a12a769ea60efd09d2
Jeff Raymakers <jray@motherduck.com>
2025-07-02 19:40:46 -0700

    core bindings for scalar functions

---
+++ bindings/test/scalar_functions.test.ts
@@ -0,0 +1,48 @@
+import duckdb from '@duckdb/node-bindings';
+import { expect, suite, test } from 'vitest';
+import { data } from './utils/expectedVectors';
+import { expectResult } from './utils/expectResult';
+import { withConnection } from './utils/withConnection';
+
+suite('scalar functions', () => {
+  test('create', () => {
+    const scalar_function = duckdb.create_scalar_function();
+    expect(scalar_function).toBeTruthy();
+  });
+  test('set name', () => {
+    const scalar_function = duckdb.create_scalar_function();
+    duckdb.scalar_function_set_name(scalar_function, 'my_func');
+  });
+  test('set return type', () => {
+    const scalar_function = duckdb.create_scalar_function();
+    const int_type = duckdb.create_logical_type(duckdb.Type.INTEGER);
+    duckdb.scalar_function_set_return_type(scalar_function, int_type);
+  });
+  test('register & run', async () => {
+    await withConnection(async (connection) => {
+      const scalar_function = duckdb.create_scalar_function();
+      duckdb.scalar_function_set_name(scalar_function, 'my_func');
+      const int_type = duckdb.create_logical_type(duckdb.Type.VARCHAR);
+      duckdb.scalar_function_set_return_type(scalar_function, int_type);
+      duckdb.scalar_function_set_function(scalar_function, (_info, input, output) => {
+        const inputSize = duckdb.data_chunk_get_size(input);
+        for (let i = 0; i < inputSize; i++) {
+          duckdb.vector_assign_string_element(output, i, `output_${i}`);
+        }
+      });
+      duckdb.register_scalar_function(connection, scalar_function);
+
+      const result = await duckdb.query(connection, "select my_func()");
+      await expectResult(result, {
+        chunkCount: 1,
+        rowCount: 1,
+        columns: [
+          { name: 'my_func()', logicalType: { typeId: duckdb.Type.VARCHAR } }
+        ],
+        chunks: [
+          { rowCount: 1, vectors: [data(16, [true], ['output_0'])]}
+        ],
+      });
+    });
+  });
+});
