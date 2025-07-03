b8735073dfe37e0e5fd1fae85e4ca1effb2debdf
Jeff Raymakers <jray@motherduck.com>
2025-07-02 19:41:29 -0700

    remove stray console.logs from test

--- bindings/test/instance_cache.test.ts
+++ bindings/test/instance_cache.test.ts
@@ -10,18 +10,14 @@ suite('instance_cache', () => {
     const cache = duckdb.create_instance_cache();
     const db1 = await duckdb.get_or_create_from_cache(cache, '');
     expect(db1).toBeTruthy();
-    console.log(db1);
     const db2 = await duckdb.get_or_create_from_cache(cache, '');
     expect(db2).toStrictEqual(db1);
-    console.log(db2);
   });
   test('memory path', async () => {
     const cache = duckdb.create_instance_cache();
     const db1 = await duckdb.get_or_create_from_cache(cache, ':memory:');
     expect(db1).toBeTruthy();
-    console.log(db1);
     const db2 = await duckdb.get_or_create_from_cache(cache, ':memory:');
     expect(db2).toStrictEqual(db1);
-    console.log(db2);
   });
 });
