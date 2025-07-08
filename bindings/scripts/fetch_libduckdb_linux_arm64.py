7f8998eb41a14f0f9fb311ba3710b65c516327d0
Jeff Raymakers <jray@motherduck.com>
2025-07-08 08:45:50 -0700

    upgrade to duckdb 1.3.2

--- bindings/scripts/fetch_libduckdb_linux_arm64.py
+++ bindings/scripts/fetch_libduckdb_linux_arm64.py
@@ -1,7 +1,7 @@
 import os
 from fetch_libduckdb import fetch_libduckdb
 
-zip_url = "https://github.com/duckdb/duckdb/releases/download/v1.3.1/libduckdb-linux-arm64.zip"
+zip_url = "https://github.com/duckdb/duckdb/releases/download/v1.3.2/libduckdb-linux-arm64.zip"
 output_dir = os.path.join(os.path.dirname(__file__), "..", "libduckdb")
 files = [
   "duckdb.h",
