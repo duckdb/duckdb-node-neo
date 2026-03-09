5be2b6456b56663f141f9c03c0796be51b26f4ce
Jeff Raymakers <jray@motherduck.com>
2026-03-09 08:45:08 -0700

    upgrade to duckdb 1.5.0

--- bindings/test/config.test.ts
+++ bindings/test/config.test.ts
@@ -5,14 +5,18 @@ import { data } from './utils/expectedVectors';
 
 suite('config', () => {
   test('config_count', () => {
-    expect(duckdb.config_count()).toBe(202);
+    expect(duckdb.config_count()).toBe(227);
   });
   test('get_config_flag', () => {
     expect(duckdb.get_config_flag(0).name).toBe('access_mode');
-    expect(duckdb.get_config_flag(duckdb.config_count() - 1).name).toBe('variant_legacy_encoding');
+    expect(duckdb.get_config_flag(duckdb.config_count() - 1).name).toBe(
+      'unsafe_enable_version_guessing',
+    );
   });
   test('get_config_flag out of bounds', () => {
-    expect(() => duckdb.get_config_flag(-1)).toThrowError(/^Config option not found$/);
+    expect(() => duckdb.get_config_flag(-1)).toThrowError(
+      /^Config option not found$/,
+    );
   });
   test('create, set, and destroy', () => {
     const config = duckdb.create_config();
@@ -22,49 +26,58 @@ suite('config', () => {
   test('default duckdb_api without explicit config', async () => {
     const db = await duckdb.open();
     const connection = await duckdb.connect(db);
-    const result = await duckdb.query(connection, `select current_setting('duckdb_api') as duckdb_api`);
-      await expectResult(result, {
-        chunkCount: 1,
-        rowCount: 1,
-        columns: [
-          { name: 'duckdb_api', logicalType: { typeId: duckdb.Type.VARCHAR } },
-        ],
-        chunks: [
-          { rowCount: 1, vectors: [data(16, [true], ['node-neo-bindings'])]},
-        ],
-      });
+    const result = await duckdb.query(
+      connection,
+      `select current_setting('duckdb_api') as duckdb_api`,
+    );
+    await expectResult(result, {
+      chunkCount: 1,
+      rowCount: 1,
+      columns: [
+        { name: 'duckdb_api', logicalType: { typeId: duckdb.Type.VARCHAR } },
+      ],
+      chunks: [
+        { rowCount: 1, vectors: [data(16, [true], ['node-neo-bindings'])] },
+      ],
+    });
   });
   test('default duckdb_api with explicit config', async () => {
     const config = duckdb.create_config();
     const db = await duckdb.open(undefined, config);
     const connection = await duckdb.connect(db);
-    const result = await duckdb.query(connection, `select current_setting('duckdb_api') as duckdb_api`);
-      await expectResult(result, {
-        chunkCount: 1,
-        rowCount: 1,
-        columns: [
-          { name: 'duckdb_api', logicalType: { typeId: duckdb.Type.VARCHAR } },
-        ],
-        chunks: [
-          { rowCount: 1, vectors: [data(16, [true], ['node-neo-bindings'])]},
-        ],
-      });
+    const result = await duckdb.query(
+      connection,
+      `select current_setting('duckdb_api') as duckdb_api`,
+    );
+    await expectResult(result, {
+      chunkCount: 1,
+      rowCount: 1,
+      columns: [
+        { name: 'duckdb_api', logicalType: { typeId: duckdb.Type.VARCHAR } },
+      ],
+      chunks: [
+        { rowCount: 1, vectors: [data(16, [true], ['node-neo-bindings'])] },
+      ],
+    });
   });
   test('overriding duckdb_api', async () => {
     const config = duckdb.create_config();
     duckdb.set_config(config, 'duckdb_api', 'custom-duckdb-api');
     const db = await duckdb.open(undefined, config);
     const connection = await duckdb.connect(db);
-    const result = await duckdb.query(connection, `select current_setting('duckdb_api') as duckdb_api`);
-      await expectResult(result, {
-        chunkCount: 1,
-        rowCount: 1,
-        columns: [
-          { name: 'duckdb_api', logicalType: { typeId: duckdb.Type.VARCHAR } },
-        ],
-        chunks: [
-          { rowCount: 1, vectors: [data(16, [true], ['custom-duckdb-api'])]},
-        ],
-      });
+    const result = await duckdb.query(
+      connection,
+      `select current_setting('duckdb_api') as duckdb_api`,
+    );
+    await expectResult(result, {
+      chunkCount: 1,
+      rowCount: 1,
+      columns: [
+        { name: 'duckdb_api', logicalType: { typeId: duckdb.Type.VARCHAR } },
+      ],
+      chunks: [
+        { rowCount: 1, vectors: [data(16, [true], ['custom-duckdb-api'])] },
+      ],
+    });
   });
 });
