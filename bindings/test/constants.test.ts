b46f0259c6399351a74794fa737e513e6b5c8f71
Jeff Raymakers <jray@motherduck.com>
2026-07-22 11:00:17 -0700

    upgrade to duckdb 1.5.5

--- bindings/test/constants.test.ts
+++ bindings/test/constants.test.ts
@@ -6,7 +6,7 @@ suite('constants', () => {
     expect(duckdb.sizeof_bool).toBe(1);
   });
   test('library_version', () => {
-    expect(duckdb.library_version()).toBe('v1.5.4');
+    expect(duckdb.library_version()).toBe('v1.5.5');
   });
   test('vector_size', () => {
     expect(duckdb.vector_size()).toBe(2048);
